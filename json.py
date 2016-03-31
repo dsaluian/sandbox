b2bb46e12ed1185ef00fe3adab9052c2e87a8980
Denys Saluian <denys.saluian@gmail.com>
2016-03-31 15:03:37 +0300

    json.py example

---
+++ json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json, requests
+
+url = 'http://maps.googleapis.com/maps/api/directions/json'
+
+params = dict(
+    origin='Chicago,IL',
+    destination='Los+Angeles,CA',
+    waypoints='Joplin,MO|Oklahoma+City,OK',
+    sensor='false'
+)
+
+resp = requests.get(url=url, params=params)
+data = json.loads(resp.text)
